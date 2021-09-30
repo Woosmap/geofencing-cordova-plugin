30e5c1ef80476b95e7318dbbd475cccb34891ace
Sameer Dhulap <sameer.dhulap@sanginfo.com>
2021-09-30 22:49:12 +0530

    corrected error info

--- src/ios/WoosmapGeofenceMessage.swift
+++ src/ios/WoosmapGeofenceMessage.swift
@@ -27,5 +27,5 @@ internal struct WoosmapGeofenceMessage {
     static let locationDeleted = "Deleted"
     static let regionDeleted = "Deleted"
     static let invalidProfile = "Invalid profile"
-    static let invalidPOIRadius = "Invalid POI Radius"
+    static let invalidPOIRadius = "POI Radius should be integer or string"
 }
